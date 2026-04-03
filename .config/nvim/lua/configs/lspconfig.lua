ae9afd3642efe1fc1a51605f0a83e58f00e7008b
josh <joshua.childs01@outlook.com>
2026-04-03 17:40:26 +1100

    lsp config

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,17 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" , "clangd", "pylsp", "texlab", "gopls", "hls", "rust_analyzer"}
+local servers = { 
+    "html", 
+    "cssls", 
+    "clangd", 
+    "pylsp", 
+    "texlab", 
+    "gopls", 
+    "hls", 
+    "rust_analyzer", 
+    "ts_ls"
+}
+
 vim.lsp.enable(servers)
 
 -- to configure lsps further read :h vim.lsp.config
