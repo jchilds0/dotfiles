47fc6d1edae7f83bfa2e9ad5d19d531434840aba
Josh <github.cornbread050@passmail.net>
2026-05-02 18:21:22 +1000

    hyprland lua config

---
+++ .config/hypr/apps.lua
@@ -0,0 +1,13 @@
+---------------------
+---- MY PROGRAMS ----
+---------------------
+
+-- Set programs that you use
+local M = {
+    terminal    = "kitty",
+    fileManager = "nautilus",
+    menu        = "wofi -I -m -a --show=drun -p Application",
+}
+
+return M
+
